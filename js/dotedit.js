6601bc02822bbbff6d27f0eb057d6a944127984b
MrMallIronmaker <725mrm@gmail.com>
2022-10-18 15:22:50 -0700

    Added the 'add tag' input, not functional yet

--- js/dotedit.js
+++ js/dotedit.js
@@ -1,8 +1,6 @@
 
 // given a match32 string, pull out the relevant tags:
 
-// import textColor from "./colors";
-
 /**
  * tag database structure:
  * 'assignments' : {
@@ -26,7 +24,7 @@ const assignments = {
 const tags = {
 	0 : {
 		"name" : "Test tag 01",
-		"shortName" : "FFF",
+		"shortName" : "",
 		"color" : "#ffaef1"
 	},
 	1 : {
@@ -64,6 +62,36 @@ function constructMatchTags(match32) {
 // UI flow:
 // edit -> add x's, add [ form ] for new or add tag
 
+/**
+ * tag assigment
+ * 1) find where tags should go
+ * 2) add in tag element
+ * 3) add in tags
+ * 4) open editing of tag assignment
+ * 5) remove tag assignment
+ * 6) add tag assignment
+ *   a) DONE - Add input with dropdown / autocomplete
+ *
+ *   DONE up to here.
+ *
+ *   b) have dropdowns be from tags
+ *   c) add tag info once selected
+ * 7) save tag assignment to JSON
+ * 8) save tag assignment to chrome.sync
+ * 9) refactor tag assignment
+ *
+ * tags
+ * 1) display tag info (n matches, name, short name, color)
+ * 2) edit tag info
+ * 3) make new tag
+ * 4) delete old tags
+ * 5) handle tag errors
+ *
+ * features
+ * 1) hide ancestry tags?
+ * 2) hide some angel tags?
+ */
+
 // wing shows ad tags
 
 // bad table for name, edit name, color, etc, and save button at the top?
@@ -92,26 +120,93 @@ function constructEditTagsButton() {
 
 	editTagsButton.onclick = function () {
 		// get all tags, and add in x-marks to them
+
 		let siblings = this.parentElement.children;
 
-		for (let i = 0; i < this.children.length; i++) {
-			let val = this.children[i];
-			if (val.tagName.toLowerCase() === "img") {
-				val.src = chrome.runtime.getURL("img/check.svg");
-				val.alt = "Save Tags";
-			}
-		}
+		if (this.classList.contains("angeldots-editTagsButton")) {
+			// remove it, replace it, and do work.
+			editTagsButton.classList.remove("angeldots-editTagsButton");
+			editTagsButton.classList.add("angeldots-saveTagsButton");
 
-		for (let i = 0; i < siblings.length; i++) {
-
-			if (siblings[i].classList.contains("angeldots-tag")) {
-				const removeTagButton = document.createElement("button");
-				removeTagButton.classList.add("angeldots-removeTagButton");
-				removeTagButton.innerHTML = "<img src='" +
-					chrome.runtime.getURL("img/close.svg") +
-					"' alt='Remove Tag'/>";
-				siblings[i].append(removeTagButton);
+			// edit its image
+			for (let i = 0; i < this.children.length; i++) {
+				let val = this.children[i];
+				if (val.tagName.toLowerCase() === "img") {
+					val.src = chrome.runtime.getURL("img/check.svg");
+					val.alt = "Save Tags";
+				}
 			}
+
+			// place a form fill right after it
+			let addDatalist = document.createElement("datalist");
+			addDatalist.classList.add("angeldots-addDatalist");
+			addDatalist.id = "browsers";
+			addDatalist.innerHTML = "  <option value=\"Chrome\"></option>\n" +
+				"  <option value=\"Firefox\"></option>\n" +
+				"  <option value=\"Internet Explorer\"></option>\n" +
+				"  <option value=\"Opera\"></option>\n" +
+				"  <option value=\"Safari\"></option>\n" +
+				"  <option value=\"Microsoft Edge\"></option>\n";
+
+			this.after(addDatalist);
+
+			let addInput = document.createElement("input");
+			addInput.setAttribute('list', addDatalist.id);
+			addInput.classList.add("angeldots-addInput");
+			addInput.id = "foobar";
+			addInput.name = "foobar2";
+			addInput.onchange = function() {
+				// check whether it's a valid entry (use a JSON thing?)
+				console.log("onchange");
+			}
+			addInput.oninput = function() {
+				console.log("oninput");
+			}
+			this.after(addInput);
+
+			// find its siblings, and give them all (x) buttons
+			for (let i = 0; i < siblings.length; i++) {
+
+				if (siblings[i].classList.contains("angeldots-tag")) {
+					const removeTagButton = document.createElement("button");
+					removeTagButton.classList.add("angeldots-removeTagButton");
+					removeTagButton.innerHTML = "<img src='" +
+						chrome.runtime.getURL("img/close.svg") +
+						"' alt='Remove Tag'/>";
+					removeTagButton.onclick = function() {
+						this.parentElement.remove();
+					}
+					siblings[i].append(removeTagButton);
+				}
+			}
+
+		} else {
+			editTagsButton.classList.remove("angeldots-saveTagsButton");
+			editTagsButton.classList.add("angeldots-editTagsButton");
+
+			// edit its image
+			for (let i = 0; i < this.children.length; i++) {
+				let val = this.children[i];
+				if (val.tagName.toLowerCase() === "img") {
+					val.src = chrome.runtime.getURL("img/wing.svg");
+					val.alt = "Edit Tags";
+				}
+			}
+
+			// find its siblings, delete all the x buttons
+			for (let i = 0; i < siblings.length; i++) {
+				if (siblings[i].classList.contains("angeldots-tag")) {
+					for (let j = 0; j < siblings[i].children.length; j++) {
+						if (siblings[i].children[j].classList.contains("angeldots-removeTagButton")) {
+							siblings[i].children[j].remove();
+						}
+					}
+				} else if (siblings[i].classList.contains("angeldots-addInput")) {
+					siblings[i].remove();
+				}
+			}
+
+			// save the data
 		}
 	};
 	return editTagsButton;
