f69b52bc5ab4c7338a71a3f0e88e776684d9b68b
MrMallIronmaker <725mrm@gmail.com>
2022-10-17 20:39:37 -0700

    removed dead / experimental code

--- dotedit.js
+++ dotedit.js
@@ -1,39 +1,7 @@
-//dd let groupcollections = document.getElementsByClassName("indicatorGroupCollection");
-// console.log(groupcollections.length);
-//
-
-var matchDict = {};
-
-function countDict(d, l) {
-	for (var i = 0; i < l.length; i++) {
-		if (l[i] in d) {
-			d[l[i]] += 1;
-		} else {
-			d[l[i]] = 1;
-		}
-	}
-}
-
-var observer = new MutationObserver(function(mutations) {
+let observer = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
 
 		switch(mutation.type) {
-			case 'childList':
-				//console.log(mutation.addedNodes[0]);
-				for (var i = 0; i < mutation.addedNodes.length; i++) {
-					if (mutation.addedNodes[i].classList) {
-						//if (mutation.addedNodes[i].id.match("match[0-9a-f]")) {
-						//	console.log(mutation.addedNodes[i].id)
-						//}
-						countDict(matchDict, mutation.addedNodes[i].classList);
-
-						//console.log(mutation.addedNodes[i].classList);
-						if (mutation.addedNodes[i].classList.contains("matchGrid")) {
-							mutation.addedNodes[i].append("<div>SOUND CHECK</div>");
-						}
-					}
-				}
-				break;
 			case "attributes":
 				let target = mutation.target;
 				let classes = target.classList;
@@ -51,7 +19,7 @@ var observer = new MutationObserver(function(mutations) {
 	})
 	//console.log(matchDict);
 });
-observer.observe(document.documentElement, { childList: true , subtree: true, attributes: true, attributeFilter: ["class"]});
+observer.observe(document.documentElement, {subtree: true, attributes: true, attributeFilter: ["class"]});
 //console.log(insertedNodes);
 
 
