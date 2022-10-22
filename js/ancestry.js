1d3a3b1d981173e46890b2964798cb501dfb7d40
MrMallIronmaker <725mrm@gmail.com>
2022-10-21 22:09:20 -0700

    added rendering on the match page, but it looks like crap

--- js/ancestry.js
+++ js/ancestry.js
@@ -1,7 +1,30 @@
 function getMatchID(target) {
-    return target.id.slice(-32);
+    if (isComparePageTarget(target)) {
+        return getComparePageMatchID();
+    } else {
+        return target.id.slice(-32);
+    }
 }
 
 function doesNeedTagContainer(target) {
-    return target.classList.contains("matchGrid");
+    // main page, regular matches
+    return target.classList.contains("matchGrid") ||
+        // compare page
+        target.classList.contains("headerBottom");
+}
+
+function isComparePageTarget(target) {
+    return target.classList.contains("headerBottom");
+}
+
+function isComparePage() {
+    const currentURL = window.location.href;
+    return /www\.ancestry\.com\/discoveryui-matches\/compare/.test(currentURL);
+}
+
+function getComparePageMatchID() {
+    const currentURL = window.location.href;
+    const regex = /https:\/\/www\.ancestry\.com\/discoveryui-matches\/compare\/[A-Fa-f0-9\-]{36}\/with\/([A-Fa-f0-9\-]{36})/;
+    const otherID = regex.exec(currentURL)[1];
+    return otherID.replace(/-/g, '').toLowerCase();
 }
